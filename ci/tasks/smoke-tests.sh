28d16093b29a8999fea004ed10bce4b737cdc401
Daniel Jones <daniel.jones@engineerbetter.com>
2017-10-31 16:11:54 +0000

    add old prezzos to blog mappings

--- ci/tasks/smoke-tests.sh
+++ ci/tasks/smoke-tests.sh
@@ -13,3 +13,5 @@ assert_non_error_response /why-eb.html
 assert_non_error_response /2017/05/03/introducing-concourse-up.html
 assert_non_error_response /2016/09/20/engineerbetter-hazelcast-cloud-foundry.html
 assert_non_error_response /update/2015/05/15/cf-summit-2015-themes.html
+assert_non_error_response /brain-aligned-delivery
+assert_non_error_response /7-stages-of-bosh

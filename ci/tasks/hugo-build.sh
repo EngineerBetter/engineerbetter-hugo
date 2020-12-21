32d7c540f0e02e6b9b9845727b674ab0e602a977
Colin Simmons <colin.simmons@engineerbetter.com>
2020-12-21 14:54:27 +0000

    Modify deployment pipeline to work with EBCF

--- ci/tasks/hugo-build.sh
+++ ci/tasks/hugo-build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
 set -eu
 
 hugo --destination ../generated-site
+
+cp ci/assets/buildpack.yml ../generated-site/
 
 pushd ../generated-site
   tar -czf ../tarball/eb-hugo-built.tar.gz .

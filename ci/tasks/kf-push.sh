67f2e987b82fe2ade44321fe4fe61ecb45706e6e
Peter Ellis Jones <pete@peterellisjones.com>
2021-01-06 12:44:46 +0000

    k8s cluster is now a regional, not zonal cluster

--- ci/tasks/kf-push.sh
+++ ci/tasks/kf-push.sh
@@ -8,6 +8,6 @@ apt-get install -y jq
 gcloud config set project "$(jq -er '.project_id' creds.json)"
 gcloud auth activate-service-account --key-file=creds.json
 
-gcloud container clusters get-credentials ebkf --zone europe-west2-b
+gcloud container clusters get-credentials ebkf --region europe-west2
 
 kf push --space $SPACE --manifest eb-hugo/${MANIFEST}

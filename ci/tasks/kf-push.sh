56c19b66bee42d542e65641dee95300a9bf45d09
Peter Ellis Jones <pete@peterellisjones.com>
2021-01-06 12:07:59 +0000

    gcloud requires GOOGLE_APPLICATION_CREDENTIALS env var in kf-push task

--- ci/tasks/kf-push.sh
+++ ci/tasks/kf-push.sh
@@ -2,6 +2,9 @@
 
 set -eu
 
+echo "${GOOGLE_CREDENTIALS}" >creds.json
+export GOOGLE_APPLICATION_CREDENTIALS=creds.json
+
 gcloud container clusters get-credentials ebkf --zone europe-west2-b
 
 kf push --space $SPACE --manifest eb-hugo/${MANIFEST}

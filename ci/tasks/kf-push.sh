9d5f48c3a88517b9975238725807eb73db08cbf6
Will Gant <w_gant@protonmail.com>
2021-01-06 12:22:51 +0000

    Install jq in container so we can extract project ID from json creds

--- ci/tasks/kf-push.sh
+++ ci/tasks/kf-push.sh
@@ -3,6 +3,8 @@
 set -eu
 
 echo "${GOOGLE_CREDENTIALS}" >creds.json
+apt-get update
+apt-get install -y jq
 gcloud config set project "$(jq -ner '.project_id' creds.json)"
 gcloud auth activate-service-account --key-file=creds.json
 

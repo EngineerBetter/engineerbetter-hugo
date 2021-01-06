dd78b57834619552cac204873e18662c06ba805d
Peter Ellis Jones <pete@peterellisjones.com>
2021-01-06 12:17:29 +0000

    Set gcloud project ID before running gcloud auth in kf-push

--- ci/tasks/kf-push.sh
+++ ci/tasks/kf-push.sh
@@ -3,6 +3,7 @@
 set -eu
 
 echo "${GOOGLE_CREDENTIALS}" >creds.json
+gcloud config set project "$(jq -ner '.project_id' creds.json)"
 gcloud auth activate-service-account --key-file=creds.json
 
 gcloud container clusters get-credentials ebkf --zone europe-west2-b

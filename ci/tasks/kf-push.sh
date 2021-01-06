451f228dd104626ce540d3f83e7449f310c073ce
Peter Ellis Jones <pete@peterellisjones.com>
2021-01-06 12:40:20 +0000

    Fix jq syntax error

--- ci/tasks/kf-push.sh
+++ ci/tasks/kf-push.sh
@@ -5,7 +5,7 @@ set -eu
 echo "${GOOGLE_CREDENTIALS}" >creds.json
 apt-get update
 apt-get install -y jq
-gcloud config set project "$(jq -ner '.project_id' creds.json)"
+gcloud config set project "$(jq -er '.project_id' creds.json)"
 gcloud auth activate-service-account --key-file=creds.json
 
 gcloud container clusters get-credentials ebkf --zone europe-west2-b
